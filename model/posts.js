2096bedf075b3f93cbe7c83b379385c2bf2ae9df
Luís Felipe <luisf.csdev@gmail.com>
2022-09-28 04:38:32 -0300

    Add code and readme

---
+++ model/posts.js
@@ -0,0 +1,23 @@
+module.exports = {
+    
+    posts: [
+        {
+            id: "anyid",
+            title: "board test",
+            description: "test description "
+        },
+    ],
+
+    getAll() {
+        return this.posts;
+    },
+
+    newPost(title, description) {
+        this.posts.push({ id: generateID(), title, description });
+    },
+
+}
+
+function generateID() {
+    return Math.random().toString(36).substring(2, 9)
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+<div align="center">
+<h1> Notice Board </h1>
+A project of a notice board with Node.js!
+</div>
+<br>
+<p align="center"><strong>🚀🚧 Project in development... 🪐🚧</strong></p>
